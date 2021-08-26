dfdd7e4f8182359565e22555056752d95aa9a9a9
stejul <stejul@rocketmail.com>
2021-08-26 21:17:43 +0200

    wip

--- src/dags/upload_to_s3.py
+++ src/dags/upload_to_s3.py
@@ -1,27 +1,20 @@
-from dagster import (
-    Any,
-    String,
-    Field,
-    Bool,
-    LocalFileHandle,
-    OutputDefinition,
-    solid,
-    pipeline,
-)
+from dagster import FileHandle, String, Output, OutputDefinition, solid, pipeline
 from dagster_aws.s3 import S3Coordinate
+from typing import List
+from os import walk
 import boto3
 import os
 
 
-@solid(name="uploadObjectToS3", description="Upload given file to S3 server")
+@solid(
+    name="uploadObjectToS3",
+    output_defs=[OutputDefinition(dagster_type=S3Coordinate, name="s3Upload")],
+    description="Upload given file to S3 server",
+)
 def upload_to_s3(
-    context, local_file: LocalFileHandle, s3_coordinate: S3Coordinate
+    context, local_files: List[str], s3_coordinate: S3Coordinate
 ) -> S3Coordinate:
 
-    return_s3_coordinate: S3Coordinate = {
-        "bucket": s3_coordinat["bucket"],
-        "key": s3_coordiante["key"] + "/" + os.path.basename(local_file.path),
-    }
     s3 = boto3.client(
         service_name="s3",
         endpoint_url="http://localhost:9000",
@@ -29,17 +22,39 @@ def upload_to_s3(
         aws_secret_access_key="password",
     )
 
-    s3.create_bucket(Bucket=return_s3_coordinate["bucket"])
-
-    s3.upload_file(
-        Filename=local_file,
-        Bucket=return_s3_coordinate["bucket"],
-        Key=return_s3_coordinate["key"],
+    s3Resource = boto3.resource(
+        service_name="s3",
+        endpoint_url="http://localhost:9000",
+        aws_access_key_id="admin",
+        aws_secret_access_key="password",
     )
-    context.log.info("Uploaded successfully")
-    return s3_coordinate
+
+    for file in local_files:
+        return_s3_coordinate: S3Coordinate = {
+            "bucket": s3_coordinate["bucket"],
+            "key": s3_coordinate["key"] + "/" + file,
+        }
+
+        if s3Resource.Bucket(return_s3_coordinate["bucket"]).creation_date is None:
+            s3.create_bucket(Bucket=return_s3_coordinate["bucket"])
+
+        context.log.info(file)
+        s3.upload_file(
+            Filename=file,
+            Bucket=return_s3_coordinate["bucket"],
+            Key=return_s3_coordinate["key"],
+        )
+        context.log.info("Uploaded successfully")
+        return s3_coordinate
+    return None
+
+
+@solid(name="getListOfFiles")
+def get_all_csv_files(context) -> str:
+    for (dirpath, dirname, filenames) in walk("src/data/"):
+        yield Output(f"src/data/{filenames}")
 
 
 @pipeline
 def execute_pipeline():
-    upload_to_s3()
+    upload_to_s3(local_files=get_all_csv_files())
